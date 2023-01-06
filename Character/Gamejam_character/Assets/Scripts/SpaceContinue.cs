f5b728e65c6d3a53d363bf4bbb9642e99fc8c763
echo2045 <forkannafis@gmail.com>
2023-01-07 04:38:12 +0600

    Nafis Commit II
    FULL UI SYSTEM DONE
    -start
    -end
    -instructions
    -credits
    
    Level Design
    -Most of lvl 2 and 3 done
    
    everything done except finishing 2 levels off.
    
    Then we add sound.

---
+++ Character/Gamejam_character/Assets/Scripts/SpaceContinue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SpaceContinue : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+}
---
+++ Character/Gamejam_character/Assets/Scripts/ToInstructions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToInstructions : MonoBehaviour
+{
+    public void ToInstructionsScene()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
